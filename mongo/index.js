6d624956fdfcebc2fe4c977e4f6a0d57229af2de
abtuhin <abtuhin@bitbucket.org>
2017-03-15 11:40:10 +0600

    post mehod via postman

--- mongo/index.js
+++ mongo/index.js
@@ -1,3 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser'); // take json and convert to object based on request
+
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  // console.log(req.body);
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+// app.get('/todos', (req, res) => {
+//
+// });
+
+app.listen(3000, () => {
+  console.log("Server Is Up And Running on Port 3000");
+});
