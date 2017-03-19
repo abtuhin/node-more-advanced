f6036df2f083e25c683577f9d2613f04688e6b37
abtuhin <abtuhin@bitbucket.org>
2017-03-19 12:33:56 +0600

    add POST /users route with authitication

--- mongo/index.js
+++ mongo/index.js
@@ -100,6 +100,24 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+//Routes for creating new user
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body); //body already a object
+
+  user.save().then((user) => {
+    // res.send(user);
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server Is Up And Running on Port ${port}`);
 });
