fbcf70e4d7a5e0977fcdc918bf91828f4546d838
abtuhin <abtuhin@bitbucket.org>
2017-03-25 11:34:51 +0600

    user login with authintication then respond back from the existing user

--- mongo/models/user.js
+++ mongo/models/user.js
@@ -77,7 +77,28 @@ UserSchema.statics.findByToken = function(token){
 
 }
 
-//Schema middleware
+// model methods get called with Uppercase User
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User  = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user){
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        }else{
+          reject();
+        }
+      });
+    });
+  });
+};
+
+//Schema middleware // document method for every user
 UserSchema.pre('save', function( next){
   var user = this;
   if(user.isModified('password')){
--- mongo/tests/index.test.js
+++ mongo/tests/index.test.js
@@ -4,25 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../index');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed.js');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First todo test'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second todo test',
-    completed: true,
-    completedAt: 333
-  }
-];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() =>{
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
